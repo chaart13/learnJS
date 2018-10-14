5883b14886d297fc15b0623e7daa414551d419c1
chaart13 <chaart13@gmail.com>
2018-10-14 17:50:28 +0300

    add strings2

--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,15 @@
 
 // strings.1
 function ucFirst(str) {
-    return str[0].toUpperCase() + str.substring(1)
+    return str[0].toUpperCase() + str.substring(1);
 }
 
 // strings.2
-var checkSpam(str) {
+function checkSpam(str) {
     if (~str.indexOf('XXX') || ~str.indexOf('viagra')) {
-        return true
+        return true;
     }
     return false
 }
+
+// strings.3
