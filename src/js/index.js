fa485da984ed74277e6a6620653e9deef95dba23
chaart13 <chaart13@gmail.com>
2018-10-16 19:48:03 +0300

    added adjustments to arrays1

--- src/js/index.js
+++ src/js/index.js
@@ -2,24 +2,18 @@
 
 // strings.1
 function ucFirst(str) {
-    return str[0].toUpperCase() + str.substring(1);
+    return str[0].toUpperCase() + str.slice(1);
 }
 
 // strings.2
 function checkSpam(str) {
-    if (~str.indexOf('XXX') || ~str.indexOf('viagra')) {
-        return true;
-    }
-    return false;
+    return !!(~str.indexOf('XXX') || ~str.indexOf('viagra'));
 }
 
 // strings.3
 function truncate(str, maxlength) {
     var sub = '...';
-    if (str.length > maxlength) {
-        return str.substring(0, maxlength - sub.length) + sub;
-    }
-    return str;
+    return (str.length > maxlength) ? str.slice(0, maxlength - sub.length) + sub : str;
 }
 
 // strings.4
@@ -37,10 +31,7 @@ delete user.name;
 
 // obj2.1
 function isEmpty(obj) {
-    if (Object.keys(obj).length > 0) {
-        return false;
-    }
-    return true;
+    return !Object.keys(obj).length;
 }
 
 var user = {};
@@ -64,18 +55,15 @@ var salaries = {
 
 // obj2.3
 function maxSalary(obj) {
-    if (Object.keys(obj).length > 0) {
-        var maxValue = 0;
-        var maxName;
-        for (var keys in obj) {
-            if (obj[keys] > maxValue) {
-                maxValue = obj[keys];
-                maxName = keys;
-            }
+    var maxValue = 0;
+    var maxName = 'нет сотрудников';
+    for (var keys in obj) {
+        if (obj[keys] > maxValue) {
+            maxValue = obj[keys];
+            maxName = keys;
         }
-        return maxName;
     }
-    return 'нет сотрудников';
+    return maxName;
 }
 
 // obj2.4
@@ -100,25 +88,36 @@ var menu = {
 multiplyNumeric(menu);
 console.log(menu.width);
 
-// arrays.1
-var goods = [];
-var lastElement = goods[goods.lenght - 1];
+// arrays1.1
+function lastElement(arr) {
+    return goods[goods.lenght - 1];
+}
+var goods = [5465, 'iho'];
+console.log(lastElement(goods));
 
-// arrays.2
-goods[goods.length] = 'Компьютер';
+// arrays1.2
+function addingSmthToTheEnd(arr, smth) {
+    arr.push(smth);
+}
+var element = 'Компьютер';
+addingSmthToTheEnd(arr, element);
 
-// arrays.3
+// arrays1.3
+// в функцию это оборачивать нет смысла
 var styles = ['Джаз', 'Блюз'];
 styles.push('Рок-н-Ролл');
 styles[styles.length - 2] = 'Классика';
 console.log(styles.shift());
 styles.unshift('Рэп', 'Регги');
 
-// arrays.4
-var arr = ['Яблоко', 'Апельсин', 'Груша', 'Лимон'];
-console.log(arr[Math.floor(Math.random() * arr.length)])
+// arrays1.4
+function getRandom(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+var fruits = ['Яблоко', 'Апельсин', 'Груша', 'Лимон'];
+console.log(getRandom(fruits));
 
-// arrays.5
+// arrays1.5
 /*
 var summPrompt = [];
 var temp;
@@ -134,8 +133,8 @@ for (var i = 0; i < summPrompt.length; i++) {
 alert(temp);
 */
 
-// arrays.6 - устно
-// arrays.7
+// arrays1.6 - устно
+// arrays1.7
 function find(arr, value) {
     for (var i = 0; i < arr.length; i++) {
         if (arr[i] == value) return i;
@@ -145,12 +144,12 @@ function find(arr, value) {
 
 var arrFor7 = ['test', 2, 1.5, false];
 
-//arrays.8
+//arrays1.8
 function filterRange(arr, a, b) {
     var newArr = []
     for (var i = 0; i < arr.length; i++) {
         if (arr[i] >= a && arr[i] <= b) {
-            newArr[newArr.length] = arr[i];
+            newArr.push(arr[i]);
         }
     }
     return newArr;
@@ -159,7 +158,7 @@ var arrFor8 = [5, 4, 3, 8, 0];
 var filtered = filterRange(arrFor8, 3, 5);
 console.log(filtered);
 
-// arrays.9
+// arrays1.9
 function Eratosphen(n) {
     var j = 0;
     var p = 2;
@@ -186,4 +185,39 @@ function Eratosphen(n) {
         summ += arrFor9[i];
     }
     return summ;
+}
+
+// arrays1.10
+function getMaxSubSum(arr) {
+    var summ = {
+        currSumm: 0,
+        resultSumm: 0,
+        lowElement: 0,
+        highElement: arr.length - 1
+    }
+    var temp1 = 0, temp2 = 0, temp3 = arr.length;
+
+    while (temp3 !== 0) {
+        for (var i = summ.lowElement; i <= summ.highElement; i++) {
+            summ.currSumm += arr[i];
+            if (summ.currSumm > summ.resultSumm) {
+                summ.resultSumm = summ.currSumm;
+                temp2 = i;
+            }
+        }
+
+        for (var i = summ.lowElement; i <= summ.highElement; i++) {
+            summ.currSumm -= arr[i];
+            if (summ.currSumm > summ.resultSumm) {
+                summ.resultSumm = summ.currSumm;
+                temp1 = i;
+            }
+        }
+
+        summ.lowElement = temp1;
+        summ.highElement = temp2;
+        temp3 -= 1;
+    }
+
+    return summ.resultSumm;
 }
